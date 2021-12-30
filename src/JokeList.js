cfc165bc4078b1072260230209d052aff1493e1d
Ibrahim Saleh <bob@gmail.com>
2021-12-30 16:51:59 -0500

    Store jokes in localstorage

--- src/JokeList.js
+++ src/JokeList.js
@@ -10,9 +10,16 @@ class JokeList extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { jokes: [] };
+    this.state = {
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+    };
   }
   async componentDidMount() {
+    if (this.state.jokes.length === 0) {
+      this.getJokes();
+    }
+  }
+  async getJokes() {
     let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
       const res = await axios.get('https://icanhazdadjoke.com/', {
@@ -20,8 +27,11 @@ class JokeList extends Component {
       });
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 })
     }
-    this.setState({ jokes: jokes })
+    this.setState({ jokes: jokes });
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
   }
+
+
   handleVote(id, delta) {
     this.setState(st => ({
       jokes: st.jokes.map(j => {
