f62227c21c10c43d50c90c9e4d83c7fd663a4ae2
Ibrahim Saleh <bob@gmail.com>
2021-12-30 15:40:18 -0500

    Create and style Joke component

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Joke from './Joke';
 import axios from 'axios';
+import uuid from '../node_modules/uuid/dist/v4';
 import './JokeList.css';
 
 class JokeList extends Component {
@@ -16,9 +18,19 @@ class JokeList extends Component {
       const res = await axios.get('https://icanhazdadjoke.com/', {
         headers: { Accept: 'application/json' }
       });
-      jokes.push(res.data.joke)
+      jokes.push({ id: uuid(), text: res.data.joke, votes: 0 })
     }
     this.setState({ jokes: jokes })
+  }
+  handleVote(id, delta) {
+    this.setState(st => ({
+      jokes: st.jokes.map(j => {
+        if (j.id === id) {
+          return { ...j, votes: j.votes + delta }
+        }
+        return j;
+      })
+    }));
   }
   render() {
     return (
@@ -30,7 +42,7 @@ class JokeList extends Component {
         </div>
         <div className='JokeList-jokes'>
           {this.state.jokes.map(j => (
-            <div>{j}</div>
+            <Joke key={j.id} votes={j.votes} text={j.text} upvote={() => this.handleVote(j.id, 1)} downvote={() => this.handleVote(j.id, -1)} />
           ))}
         </div>
       </div>
